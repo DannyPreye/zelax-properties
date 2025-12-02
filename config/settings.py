f326c497289f6488ed42b09d0528c897254e48d6
Daniel Olawoyin <olawoyindaniel95@gmail.com>
2025-12-02 12:16:40 +0100

    updated the allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -220,6 +220,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
+    "https://zelax-properties.fly.dev",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
